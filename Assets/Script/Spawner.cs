7c1706591a92693d5d39c2fd934427b627da8f12
CallMeLynix <159532943+CallMeLynix@users.noreply.github.com>
2025-02-24 10:01:14 +0700

    Penambahan script

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject enemyPrefab;
+    public float spawnInterval = 2f;
+    public float spawnRangeX = 10f;
+    public float spawnPosZ = 10f;
 
-    // Update is called once per frame
+    private float spawnTimer = 0f;
+
     void Update()
     {
-        
+        spawnTimer += Time.deltaTime;
+        if (spawnTimer >= spawnInterval)
+        {
+            SpawnEnemy();
+            spawnTimer = 0f;
+        }
+    }    void SpawnEnemy()
+    {
+        float spawnPosX = Random.Range(-spawnRangeX, spawnRangeX);
+        Vector3 spawnPosition = new Vector3(spawnPosX, 0f, spawnPosZ);
+        Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
     }
 }
